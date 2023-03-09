212445e043049f76533a19589d0f279efc02e56c
Berlin <berlinused@gmail.com>
2023-03-09 14:21:16 +0800

    [Controller][use][conditions]

--- app/Http/Controllers/ThursdayController.php
+++ app/Http/Controllers/ThursdayController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Thursday;
+
 class ThursdayController extends Controller
 {
     /**
@@ -11,6 +13,8 @@ class ThursdayController extends Controller
      */
     public function index()
     {
+        $data = Thursday::get();
+        dd($data);
         return view('thursday.index');
     }
 
